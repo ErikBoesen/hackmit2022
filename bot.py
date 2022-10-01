8ec2f757c690d6c848ec2204a08ad6d2d3ddb758
ErikBoesen <me@erikboesen.com>
2022-10-01 15:48:27 -0400

    Switch to modern slack_sdk package

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from distutils.command.config import config
-import slack
+import slack_sdk
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = slack.WebClient(token=os.environ['SLACK_TOCKEN'])
+client = slack_sdk.WebClient(token=os.environ['SLACK_TOKEN'])
 
 client.chat_postMessage(channel='#test', text='Hello World!')
