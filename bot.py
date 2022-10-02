a923f828ab7f0dfefeed8543575e27c4bca05336
carolynqu <67798872+carolynqu@users.noreply.github.com>
2022-10-02 02:45:08 -0400

    adding to message

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from distutils.command.config import config
 import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -13,7 +12,15 @@ client = WebClient(token=os.environ['SLACK_TOKEN'])
 
 matty = Classifier('easy_sponsor.csv')
 df = pd.read_csv('easy_student.csv')
+
+temp = pd.read_csv('easy_sponsor_info.csv')
 student1 = df.iloc[1, 1:len(df.columns)]
 matches = matty.classify_row(student1)
 result = '\n'.join(matches)
-client.chat_postMessage(channel='#hackbot', text=result)
+
+client.chat_postMessage(channel='#hackbot', text="Hey <@U044RSJTX6F>! *Check out your top 5 companies:*")
+
+for i in matches: 
+    client.chat_postMessage(channel='#hackbot', text=">" + i) 
+
+client.chat_postMessage(channel='#hackbot', text="Based on your interests we have matched you with these 5 companies. Check out their opportunities and head over to their tables soon :smile:!")
