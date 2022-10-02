165ce391e4a5c7017adfb6b2cca7c335ed58a319
Alejo <alegonshu@gmail.com>
2022-10-01 23:30:23 -0400

    print results to slack

--- bot.py
+++ bot.py
@@ -11,12 +11,9 @@ load_dotenv()
 
 client = WebClient(token=os.environ['SLACK_TOKEN'])
 
-client.chat_postMessage(channel='#hackbot', text='Hello World!')
-
-client.chat_postMessage(channel='#hackbot', text='Hello World!')
-
 matty = Classifier('easy_sponsor.csv')
-
 df = pd.read_csv('easy_student.csv')
-student1 = df.iloc[1, 1:len(df.columns)]
+student1 = df.iloc[2, 1:len(df.columns)]
 matches = matty.classify_row(student1)
+result = '\n'.join(matches)
+client.chat_postMessage(channel='#hackbot', text=result)
