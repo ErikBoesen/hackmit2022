1af7341e53fee05ce1a080e116320c0761b025f4
Alejo <alegonshu@gmail.com>
2022-10-01 23:31:49 -0400

    poopie

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ client = WebClient(token=os.environ['SLACK_TOKEN'])
 
 matty = Classifier('easy_sponsor.csv')
 df = pd.read_csv('easy_student.csv')
-student1 = df.iloc[2, 1:len(df.columns)]
+student1 = df.iloc[1, 1:len(df.columns)]
 matches = matty.classify_row(student1)
 result = '\n'.join(matches)
 client.chat_postMessage(channel='#hackbot', text=result)
